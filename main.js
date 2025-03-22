571b798e09321d633b3ec299cd6de816a34a7dd3
PAARI C <166461211+Paari15@users.noreply.github.com>
2025-03-22 14:13:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 // filepath: c:\Users\paari\Downloads\icloud-notes-launcher\main.js
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-        title: 'iCloud App',
-        icon: __dirname + '/icon.ico',
+        title: 'iCloud Notes',
+        icon: path.join(__dirname, 'icon.ico'),
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
+            sandbox: true,
         },
     });
 
     win.loadURL('https://www.icloud.com/notes/');
     win.setMenuBarVisibility(false);
+
+    win.on('closed', () => {
+        win.destroy();
+    });
 }
 
 app.on('ready', createWindow);
